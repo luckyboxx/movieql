b6fb4c050a8c7fe895d5b1fbb5d9efed97a6ba4b
luckyboxx <jo9214@gmail.com>
2021-05-18 15:05:00 +0900

    #10 Delete Mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,13 @@
-const changyeon = {
-    name: "Changyeon",
-    age: 27,
-    gender: "male"
-}
+import { getMovies, getById, addMovie, deleteMovie } from "./db";
 
 const resolvers = {
     Query: {
-        person: () => changyeon
+        movies: () => getMovies(),
+        movie: (_, { id }) => getById(id)
+    },
+    Mutation: {
+        addMovie: (_, { name, score }) => addMovie(name, score),
+        deleteMovie: (_, { id }) => deleteMovie(id)
     }
 };
 
